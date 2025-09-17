ebbc59e101342a92342e411a7099bb7003410e1d
SashaAntoshin <globeinwork@gmail.com>
2025-09-17 23:05:57 +0300

    модели рецептов

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,128 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+MAX_LENGTH = 150
+
+User = get_user_model()
+
+
+class Tag(models.Model):
+    """Модель тега рецепта"""
+    name = models.CharField(
+        max_length=MAX_LENGTH,
+        unique=True,
+        verbose_name='Тег',
+        blank=False
+    )
+    slug = models.SlugField(
+        max_length=MAX_LENGTH,
+        unique=True,
+        verbose_name='Уникальный слаг',
+        blank=False
+    )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Модель ингридиента"""
+    name = models.CharField(
+        max_length=MAX_LENGTH,
+        verbose_name='Название ингридиента'
+    )
+    unit = models.CharField(
+        max_length=MAX_LENGTH,
+        verbose_name='Единица измерения'
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.name
+
+
+class IngredientsInRecipe(models.Model):
+    "Модель для связи ингридиента и рецепта"
+    recipe = models.ForeignKey(
+        'Recipe',
+        on_delete=models.CASCADE,
+        related_name='ingredient_amounts'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='recipe_amounts'
+    )
+    amount = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1)],
+        verbose_name='Количество'
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент в рецепте'
+        verbose_name_plural = 'Ингредиенты в рецептах'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['recipe', 'ingredient'],
+                name='unique_ingredient_in_recipe'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.ingredient} в {self.recipe}'
+
+
+class Recipe(models.Model):
+    """Модель рецептов."""
+    author = models.ForeignKey(
+        User,
+        related_name='recipes',
+        on_delete=models.CASCADE,
+        verbose_name='Автор'
+    )
+    name = models.CharField(
+        max_length=MAX_LENGTH,
+        blank=False
+    )
+    image = models.ImageField(
+        upload_to='recipes/images',
+        verbose_name='Фото'
+
+    )
+    description = models.TextField(verbose_name='Оисание рецепта')
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='recipes',
+        verbose_name='Теги'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='IngredientsInRecipe',
+        verbose_name='Ингредиенты'
+    )
+    time = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1)],
+        verbose_name='Время приготовления',
+        help_text='Время в минутах'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
--- backend/users/models.py
+++ backend/users/models.py
@@ -2,19 +2,24 @@ from django.db import models
 
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-MAX_TITLE_LENGTH = 200
+MAX_LENGTH = 200
 
 
 class User(AbstractUser):
     """Модель пользователя."""
     username = models.CharField(
-        max_length=MAX_TITLE_LENGTH,
+        max_length=MAX_LENGTH,
         blank=False,
-        verbose_name='Имя',
+        verbose_name='Имя пользователя',
         help_text='Обязательное поле'
     )
+    first_name = models.CharField(
+        max_length=MAX_LENGTH,
+        verbose_name='Имя',
+        blank=False
+    )
     last_name = models.CharField(
-        max_length=MAX_TITLE_LENGTH,
+        max_length=MAX_LENGTH,
         blank=False,
         verbose_name='Фамилия',
         help_text='Обязательное поле'
@@ -27,6 +32,7 @@ class User(AbstractUser):
         help_text='Обязательное поле',
         blank=False
     )
+    avatar = models.ImageField(upload_to='users/avatars', blank=True, null=True)
     groups = None
     user_permissions = None
 
