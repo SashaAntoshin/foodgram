413a893a3325e23525f8bf2a68e3f4e9fdbf1bc0
SashaAntoshin <globeinwork@gmail.com>
2025-09-18 09:44:57 +0300

    adminzone recipes

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,29 @@
+"""Регистрацияя моделей из приложения рецептов"""
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Recipe, Ingredient, IngredientsInRecipe
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name', 'slug')
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author')
+    search_fields = ('name', 'author__username')
+    list_filter = ('tags',)
+    filter_horizontal = ('tags',)
+    
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'unit')
+    search_fields = ('name',)
+    
+
+@admin.register(IngredientsInRecipe)
+class IngredientInRecipeAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'amount')
