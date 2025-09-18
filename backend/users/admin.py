a64f35488443d4f088d2c443e3af1c6efe00e78a
SashaAntoshin <globeinwork@gmail.com>
2025-09-18 10:58:59 +0300

    adminka + form

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,14 +1,33 @@
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
-
+from django import forms
 from .models import User
+
+
+class CustomRegistratonForm(forms.ModelForm):
+    """Форма создания пользователя через админку"""
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'first_name', 'last_name', 'password')
 
 
 @admin.register(User)
 class NewUserAdmin(UserAdmin):
-	"""Кастомная админка пользователя"""
-	list_display = ('username', 'last_name', 'email', 'is_staff')
-	search_fields = ('username', 'email')
-	list_filter = ('is_staff',)
-	ordering = ('username',)
-	filter_horizontal = ()
+    """Кастомная админка пользователя"""
+    add_form = CustomRegistratonForm
+    add_fieldsets = (
+        ('Основная информация', {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'first_name', 'last_name'),
+        }),
+    )
+    search_fields = ('email', 'username')
+    list_filter = ('is_staff', 'is_active')
+    ordering = ('email',)
+    list_display = ('email', 'first_name', 'last_name', 'username')
+    filter_horizontal = []
+    fieldsets = (
+        ('Основная информация', {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('username', 'first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
