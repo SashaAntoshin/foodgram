415933cc9cab9cbd3b1724cc9ebbca708a09dcca
SashaAntoshin <globeinwork@gmail.com>
2025-09-30 23:01:48 +0300

    проект запущен в docker и работает

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -50,7 +50,11 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+	"http://127.0.0.1:3000",
+    "http://frontend:3000",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "foodgram.urls"
 
