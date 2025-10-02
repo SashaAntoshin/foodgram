962044f14281167f31640c07f6bc65701ec89122
SashaAntoshin <globeinwork@gmail.com>
2025-10-02 15:18:58 +0300

    settings static

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -139,12 +139,11 @@ USE_TZ = True
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = BASE_DIR / "collected_static"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
