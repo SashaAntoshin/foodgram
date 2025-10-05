024996a541444e78eb8cc6445222bd053cddf376
SashaAntoshin <globeinwork@gmail.com>
2025-10-05 14:29:19 +0300

    fixed settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -20,7 +20,7 @@ ALLOWED_HOSTS = os.getenv(
 ).split(",")
 
 CSRF_TRUSTED_ORIGINS = os.getenv(
-    "CSRF_TRUSTED_ORIGINS", 
+    "CSRF_TRUSTED_ORIGINS",
     "https://foodisgood.duckdns.org,http://foodisgood.duckdns.org"
 ).split(",")
 
