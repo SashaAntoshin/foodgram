4163263a0e0218cc723c3bf3dba7eff78171ad5d
SashaAntoshin <globeinwork@gmail.com>
2025-10-09 23:18:53 +0300

    убрал session

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -131,7 +131,6 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 6,
